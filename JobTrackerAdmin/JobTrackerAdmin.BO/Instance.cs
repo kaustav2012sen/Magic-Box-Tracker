b8f816c9da4c1e387fc76147dddd39eb41b83130
kaustav2012sen <kaustav.bandreal@gmail.com>
2018-06-30 00:11:48 +0530

    commit 30062018 @Kaustav 1

---
+++ JobTrackerAdmin/JobTrackerAdmin.BO/Instance.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobTrackerAdmin.BO
+{
+    public class Instance
+    {
+        public int Attr_Integer1 { get; set; }
+        public int Attr_Integer2 { get; set; }
+        public int Attr_Integer3 { get; set; }
+        public int Attr_Integer4 { get; set; }
+        public int Attr_Integer5 { get; set; }
+        public string Attr_NVarchar1 { get; set; }
+        public string Attr_NVarchar2 { get; set; }
+        public string Attr_NVarchar3 { get; set; }
+        public string Attr_NVarchar4 { get; set; }
+        public string Attr_NVarchar5 { get; set; }
+        public string Attr_NVarchar6 { get; set; }
+        public string Attr_NVarchar7 { get; set; }
+        public string Attr_NVarchar8 { get; set; }
+        public string Attr_NVarchar9 { get; set; }
+        public string Attr_NVarchar10 { get; set; }
+        public bool Attr_Bit1 { get; set; }
+        public bool Attr_Bit2 { get; set; }
+        public bool Attr_Bit3 { get; set; }
+        public bool Attr_Bit4 { get; set; }
+        public bool Attr_Bit5 { get; set; }
+        public float Attr_Float1 { get; set; }
+        public float Attr_Float2 { get; set; }
+        public float Attr_Float3 { get; set; }
+        public float Attr_Float4 { get; set; }
+        public float Attr_Float5 { get; set; }
+    }
+}
